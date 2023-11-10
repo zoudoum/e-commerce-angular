b95a5bebc352f6eb12a457910d8ba1a0f9523de6
zoudoum <doumzoro@gmail.com>
2023-11-10 00:38:19 +0000

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-
+import { Component, HostListener } from '@angular/core';
+declare const bootstrap: any;
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +7,37 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'serviceiptv';
+  screenWidth: number;
+  etat:boolean=false;
+  etatbg:string;
+  ngOnInit() {
+    
+    this.screenWidth = window.innerWidth;
+    this.etatbg='#FFA500';
+    
+  }
+ 
+  @HostListener('window:resize', ['$event'])
+  onResize(event) {
+    this.screenWidth = window.innerWidth;
+   
+  }
+  @HostListener('window:scroll', ['$event'])
+  onScroll(event: Event): void {
+    if(window.scrollY==0){
+      this.etatbg='#FFA500';
+    }
+    else{
+    this.etatbg='white';}
+    
+  }
+  onClickMenu(){
+   this.etat=true;
+  }
+  getEtat(){
+    return this.etat;
+  }
+  onClickX(){
+    this.etat=false;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { AproposComponent } from './apropos/apropos.component';
+import { InfoComponent } from './info/info.component';
+import { PhotoComponent } from './photo/photo.component';
+import { BackgoundimagesComponent } from './backgoundimages/backgoundimages.component';
+import { GalerieComponent } from './galerie/galerie.component';
+import { MenuComponent } from './menu/menu.component';
+import { FocusDirective } from './focus.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    AproposComponent,
+    InfoComponent,
+    PhotoComponent,
+    BackgoundimagesComponent,
+    GalerieComponent,
+    MenuComponent,
+    FocusDirective
   ],
   imports: [
     BrowserModule,
